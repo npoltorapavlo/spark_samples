6ecd356b07edc9de5a285a487e80fbab8661db28
Nikita Poltorapavlo <Nikita_Poltorapavlo2@cable.comcast.com>
2020-11-06 15:25:15 +0200

    simple file-upload server

---
+++ node/file-upload/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+const express = require('express')
+
+const app = express()
+
+const pathForUploads = './uploads'
+
+if (!fs.existsSync(pathForUploads)) {
+  fs.mkdirSync(pathForUploads)
+}
+
+app.get('/', (req, res) => {
+  res.send('GET')
+})
+
+app.post('/', (req, res) => {
+  res.send('POST')
+})
+
+app.put('/:filename', (req, res) => {
+  req.pipe(fs.createWriteStream(`${pathForUploads}/${req.params.filename}`))
+
+  req.on('end', () => res.status(200).end());
+})
+
+app.listen(3000)
